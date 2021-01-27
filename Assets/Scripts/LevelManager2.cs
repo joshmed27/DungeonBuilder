dc4a13b66a7d3926f453e30c8a406abdc28649bb
joshmed27 <joshuamed27@gmail.com>
2021-01-27 00:37:34 -0700

    New tile generation scripts and camera movement

---
+++ Assets/Scripts/LevelManager2.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor.Tilemaps;
+
+public class LevelManager2 : MonoBehaviour{
+
+    public TileBase tile;
+    public Tilemap mainmap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[,] mapArray = GenerateArray(100, 100, false);
+        RenderMap(mapArray, mainmap, tile);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    public static int[,] GenerateArray(int width, int height, bool empty){
+        int[,] map = new int[width, height];
+        for (int x = 0; x < map.GetUpperBound(0); x++){
+            for (int y = 0; y < map.GetUpperBound(1); y++){
+                if (empty){
+                    map[x, y] = 0;
+                }
+                else{
+                    map[x, y] = 1;
+                }
+            }
+        }
+        return map;
+    }
+
+    public static void RenderMap(int[,] map, Tilemap tilemap, TileBase tile){
+        //Clear the map (ensures we dont overlap)
+        tilemap.ClearAllTiles(); 
+        //Loop through the width of the map
+        for (int x = 0; x < map.GetUpperBound(0) ; x++){
+            //Loop through the height of the map
+            for (int y = 0; y < map.GetUpperBound(1); y++){
+                // 1 = tile, 0 = no tile
+                if (map[x, y] == 1){
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tile); 
+                }
+            }
+        }
+    }
+
+    public static void UpdateMap(int[,] map, Tilemap tilemap) {//Takes in our map and tilemap, setting null tiles where needed
+        for (int x = 0; x < map.GetUpperBound(0); x++){
+            for (int y = 0; y < map.GetUpperBound(1); y++){
+                //We are only going to update the map, rather than rendering again
+                //This is because it uses less resources to update tiles to null
+                //As opposed to re-drawing every single tile (and collision data)
+                if (map[x, y] == 0){
+                    tilemap.SetTile(new Vector3Int(x, y, 0), null);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ToMoveCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToMoveCamera : MonoBehaviour
+{
+  public Transform player;
+  public Vector3 cameraOffset;
+  public int cameraSpeed = 10;
+
+  void Start()
+  {
+
+  }
+
+  void FixedUpdate (){
+    if (Input.GetKey (KeyCode.A)) {
+        transform.Translate (Vector3.left * cameraSpeed * Time.deltaTime); 
+    }
+    if(Input.GetKey (KeyCode.D)) {
+        transform.Translate (Vector3. right *   cameraSpeed * Time.deltaTime);
+    }
+    if(Input.GetKey (KeyCode.W)) {
+        transform.Translate (Vector3. up *   cameraSpeed * Time.deltaTime);
+    }
+    if(Input.GetKey (KeyCode.S)) {
+        transform.Translate (Vector3. down *   cameraSpeed * Time.deltaTime);
+    }
+  }
+}
